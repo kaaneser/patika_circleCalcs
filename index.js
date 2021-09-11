4f28f408a51a91b7712bf83a6cec08e935cbbd26
Kaan Eser <69899907+kaaneser@users.noreply.github.com>
2021-09-11 17:08:54 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const { circleArea, circleCircumference } = require('./circle');
+
+const r = 5;
+console.log('Circle Area: ' + circleArea(r));
+console.log('Circle Circumference: ' + circleCircumference(r));
